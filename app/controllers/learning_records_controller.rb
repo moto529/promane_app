4289960156f7afcc1efad36ef40aa2bed250011c
tachikawa motoki <tachikawamotoki@tachikawamotoonoMacBook-Air.local>
2022-10-05 19:02:38 +0900

    投稿作成機能

--- app/controllers/learning_records_controller.rb
+++ app/controllers/learning_records_controller.rb
@@ -1,7 +1,21 @@
 class LearningRecordsController < ApplicationController
   
   def index
-    @learning_records = LearningRecord.all 
+    @learning_records = LearningRecord.all
+    @learning_record = LearningRecord.new
   end
 
+  def create
+    @learning_record = LearningRecord.new(learning_record_params)
+    if @learning_record.save
+      redirect_to learning_records_path
+    else
+      render "learning_records/index"
+    end
+  end
+
+  private
+  def learning_record_params
+    params.require(:learning_record).permit(:learning_method, :language, :learning_time)
+  end
 end
