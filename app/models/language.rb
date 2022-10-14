87d7bb22266820db92be55ef74a88cfdcf5c6852
tachikawa motoki <tachikawamotoki@tachikawamotoonoMacBook-Air.local>
2022-10-14 14:25:35 +0900

    uniquenessにscopeを追加

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,5 +1,5 @@
 class Language < ApplicationRecord
   belongs_to :user
   has_many :records, dependent: :destroy
-  validates :language, presence: true, uniqueness: true, length: {minimum:2, maximum:15}
+  validates :language, presence: true, uniqueness: { scope: :user_id }, length: {minimum:2, maximum:15}
 end
