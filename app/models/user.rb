13c0c57ffb22f1b4ad845c6ff21b072d71842012
tachikawa motoki <tachikawamotoki@tachikawamotoonoMacBook-Air.local>
2022-10-14 21:07:53 +0900

    recordsに関連づけ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :languages, dependent: :destroy
+  has_many :records, through: :languages
 
   validates :password, presence: true, on: :create
 
