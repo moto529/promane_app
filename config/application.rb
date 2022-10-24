f4ed04fbe63e0c948af40af60e76d1be8a8f28ba
tachikawa motoki <tachikawamotoki@tachikawamotoonoMacBook-Air.local>
2022-10-24 18:58:16 +0900

    日本語化

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module PromaneApp
     #
     config.time_zone = 'Tokyo'
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
