2e9270f64ca2f60873071ad4ecd537376cc56b8e
tachikawa motoki <tachikawamotoki@tachikawamotoonoMacBook-Air.local>
2022-10-25 22:28:24 +0900

    バリデーション変更

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,5 +2,6 @@ class Record < ApplicationRecord
   belongs_to :language
   has_one :user, through: :language
   validates :detail, presence: true, length: { maximum: 250 }
-  validates :time, presence: true, numericality: true
+  validates :learning_hours, inclusion: { in: (0..10).to_a }
+  validates :laerning_minutes, inclusion: { in: [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55] }
 end
