974acbc34c98cced4ddd8e262305be75c73b9ab6
tachikawa motoki <tachikawamotoki@tachikawamotoonoMacBook-Air.local>
2022-10-28 15:27:14 +0900

    ソート機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,14 @@ class UsersController < ApplicationController
 
   def history
     # ユーザーとlanguageに紐づいたレコードを作成降順に取得する
-    @histories = User.find(current_user.id).records.all.order(created_at: :desc)
+    if  params[:latest]
+      @histories = @users.find(current_user.id).records.latest
+    elsif params[:old]
+      @histories = @users.find(current_user.id).records.old
+    else
+      @histories = @users.find(current_user.id).records.latest
+    end
+    # @histories = @users.find(current_user.id).records.order(created_at: :desc)
   end
 
   def unsubscribe; end
--- app/models/record.rb
+++ app/models/record.rb
@@ -4,4 +4,8 @@ class Record < ApplicationRecord
   validates :detail, presence: true, length: { maximum: 250 }
   validates :learning_hours, inclusion: { in: (0..10).to_a }
   validates :laerning_minutes, inclusion: { in: [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55] }
+
+
+  scope :latest, -> {order(created_at: :desc)}
+  scope :old, -> {order(created_at: :asc)}
 end
