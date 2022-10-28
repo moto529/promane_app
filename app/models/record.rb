3f3ab6f6b6de5dc9bfc311826c482417106f4659
tachikawa motoki <tachikawamotoki@tachikawamotoonoMacBook-Air.local>
2022-10-28 22:10:49 +0900

    時間表示を修正

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,7 +2,7 @@ class Record < ApplicationRecord
   belongs_to :language
   has_one :user, through: :language
   validates :detail, presence: true, length: { maximum: 250 }
-  validates :learning_hours, inclusion: { in: (0..10).to_a }
+  validates :learning_hours, inclusion: { in: [0, 60, 120, 180, 240, 300, 360, 420, 480, 540, 600] }
   validates :laerning_minutes, inclusion: { in: [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55] }
 
 
